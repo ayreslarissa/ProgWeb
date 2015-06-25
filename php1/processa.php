03654d591eebf1edec4fd2443a7860f9c3bb6f0f
Larissa Ayres <larimayres@gmail.com>
2015-06-25 09:57:43 -0400

    Adicionando exercicio 1 de PHP

---
+++ php1/processa.php
@@ -0,0 +1,9 @@
+<?php
+	echo $_GET['nome'] . "<br>";
+		if ($_GET['genero'] == 'masculino') {
+				echo 'Masculino' . "<br>";
+		}else{
+				echo 'Feminino' . "<br>";
+		}
+	echo $_GET['comentarios'] . "<br>";
+?>
