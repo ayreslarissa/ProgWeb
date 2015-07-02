7765129032b5a4cc73807fb247fe91d038dc1474
Larissa Ayres <larimayres@gmail.com>
2015-07-01 23:48:27 -0400

    Atualizando processa.php

--- PHP3/processa.php
+++ PHP3/processa.php
@@ -4,13 +4,13 @@ $nome = $_GET['nome'];
 $genero = $_GET['genero'];
 $comentarios = $_GET['comentarios'];
 
-	echo 'Bem-vindo(a), ' . $_GET['nome'] . "!<br>";
-		if ($_GET['genero'] == 'masculino') {
+	echo 'Bem-vindo(a), ' . $nome . "!<br>";
+		if ($genero == 'masculino') {
 				echo 'Gênero: Masculino' . "<br>";
 		}else{
 				echo 'Gênero: Feminino' . "<br>";
 		}
-	echo 'Seus comentários: "' . $_GET['comentarios'] . '"' . "<br>";
+	echo 'Seus comentários: "' . $comentarios . '"' . "<br>";
 			
 ?>
 
